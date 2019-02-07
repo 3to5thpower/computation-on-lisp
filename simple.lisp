3d2453457e8ee7bb6fce8424320fe0fa19fec215
nisi3genki <nisi3genki@gmail.com>
2019-02-08 02:03:34 +0900

    言語っぽくした

--- simple.lisp
+++ simple.lisp
@@ -10,7 +10,7 @@
 ;;numクラス
 (defclass num ()
   ((value :accessor value :initarg :value)))
-(defmacro make-num (value)
+(defmacro num (value)
   `(make-instance 'num :value ,value))
 (defmethod to-s ((obj num))
   (format nil "~a" (slot-value obj 'value)))
@@ -21,7 +21,7 @@
 (defclass add ()
   ((left :accessor left :initarg :left)
    (right :accessor right :initarg :right)))
-(defmacro make-add (left right)
+(defmacro add (left right)
   `(make-instance 'add :left ,left :right ,right))
 (defmethod to-s ((obj add))
   (format nil "~a + ~a"
@@ -31,16 +31,16 @@
 (defmethod reduction ((obj add) &optional environment)
   (cond
     ((reduciblep (left obj))
-     (make-add (reduction (left obj) environment) (right obj)))
+     (add (reduction (left obj) environment) (right obj)))
     ((reduciblep (right obj))
-     (make-add (left obj) (reduction (right obj) environment)))
-    (t (make-num (+ (value (left obj)) (value (right obj)))))))
+     (add (left obj) (reduction (right obj) environment)))
+    (t (num (+ (value (left obj)) (value (right obj)))))))
 
 ;;multiplyクラス
 (defclass multiply ()
   ((left :accessor left :initarg :left)
    (right :accessor right :initarg :right)))
-(defmacro make-multiply (left right)
+(defmacro multiply (left right)
   `(make-instance 'multiply :left ,left :right ,right))
 (defmethod to-s ((obj multiply))
   (format nil "~a * ~a"
@@ -50,25 +50,27 @@
 (defmethod reduction ((obj multiply) &optional environment)
   (cond
     ((reduciblep (left obj))
-     (make-multiply (reduction (left obj) environment) (right obj)))
+     (multiply (reduction (left obj) environment) (right obj)))
     ((reduciblep (right obj))
-     (make-multiply (left obj) (reduction (right obj) environment)))
-    (t (make-num (* (value (left obj)) (value (right obj)))))))
+     (multiply (left obj) (reduction (right obj) environment)))
+    (t (num (* (value (left obj)) (value (right obj)))))))
 
 ;;boolクラス
 (defclass bool ()
   ((value :accessor value :initarg :value)))
-(defmacro make-bool (value)
+(defmacro bool (value)
   `(make-instance 'bool :value ,value))
 (defmethod to-s ((obj bool))
-  (format nil "~a" (value obj)))
+  (if (value obj)
+      (format nil "true")
+      (format nil "false")))
 (defmethod reduciblep ((obj bool)) nil)
 
 ;;lessthanクラス
 (defclass lessthan ()
   ((left :accessor left :initarg :left)
    (right :accessor right :initarg :right)))
-(defmacro make-lessthan (left right)
+(defmacro lessthan (left right)
   `(make-instance 'lessthan :left ,left :right ,right))
 (defmethod to-s ((obj lessthan))
   (format nil "(~a < ~a)"
@@ -78,15 +80,15 @@
 (defmethod reduction ((obj lessthan) &optional environment)
   (cond
     ((reduciblep (left obj))
-     (make-lessthan (reduction (left obj) environment) (right obj)))
+     (lessthan (reduction (left obj) environment) (right obj)))
     ((reduciblep (right obj))
-     (make-lessthan (left obj) (reduction (right obj) environment)))
-    (t (make-bool (< (value (left obj)) (value (right obj)))))))
+     (lessthan (left obj) (reduction (right obj) environment)))
+    (t (bool (< (value (left obj)) (value (right obj)))))))
 
 ;;varablクラス(変数)
 (defclass varabl ()
   ((name :accessor name :initarg :name)))
-(defmacro make-varabl (name)
+(defmacro var (name)
   `(make-instance 'varabl :name ',name))
 (defmethod to-s ((obj varabl))
   (format nil "~a" (name obj)))
@@ -96,18 +98,19 @@
 
 ;;環境を生成する関数(make-env 'x 1 'y 2 ...)のように利用
 (defun make-one-env (var val)
-  (cons var (make-num val)))
+  (cons var (num val)))
 (defun make-env (var val &rest body)
   (do ((bodylst body (cddr bodylst))
        (varname var (first bodylst))
        (val val (second bodylst))
        (lst nil (cons (make-one-env varname val) lst)))
       ((null bodylst) (nreverse (cons (make-one-env varname val) lst)))))
-(defmacro one-varabl-env (var val)
+(defmacro env1 (var val)
   `(make-env ',var ,val))
-(defmacro two-varabl-env (var1 val1 var2 val2)
+(defmacro env2 (var1 val1 var2 val2)
   `(nconc (make-env ',var1 ,val1)
           (make-env ',var2 ,val2)))
+
 
 ;;抽象機械本体
 (defclass machine ()
@@ -147,7 +150,7 @@
 (defclass assign ()
   ((name :accessor name :initarg :name)
    (expression :accessor expression :initarg :expression)))
-(defmacro make-assign (name expression)
+(defmacro set! (name expression)
   `(make-instance 'assign :name ',name :expression ,expression))
 (defmethod to-s ((obj assign))
   (format nil "~a = ~a" (name obj) (to-s (expression obj))))
@@ -178,7 +181,7 @@
           (to-s (condp obj))
           (to-s (consequence obj))
           (to-s (alternative obj))))
-(defmacro make-if-class (condp conseq alter)
+(defmacro if! (condp conseq alter)
   `(make-instance 'if-class
                   :condition ,condp
                   :consequence ,conseq
@@ -240,13 +243,13 @@
 (defmethod evaluater ((obj varabl) &optional environment)
   (cdr (assoc (name obj) environment)))
 (defmethod evaluater ((obj add) &optional environment)
-  (make-num (+ (value (evaluater (left obj) environment))
+  (num (+ (value (evaluater (left obj) environment))
                (value (evaluater (right obj) environment)))))
 (defmethod evaluater ((obj multiply) &optional environment)
-  (make-num (* (value (evaluater (left obj) environment))
+  (num (* (value (evaluater (left obj) environment))
                (value (evaluater (right obj) environment)))))
 (defmethod evaluater ((obj lessthan) &optional environment)
-  (make-bool (< (value (evaluater (left obj) environment))
+  (bool (< (value (evaluater (left obj) environment))
                 (value (evaluater (right obj) environment)))))
 (defun evaluate (obj &optional environment)
   (show (evaluater obj environment)))
