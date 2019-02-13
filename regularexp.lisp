5bdc578e0f01b44b8261d92867155734c3bc7d7e
nisi3genki <nisi3genki@gmail.com>
2019-02-14 02:35:31 +0900

    正規表現を抽象構文木で表現するためのクラスの定義が完了

--- regularexp.lisp
+++ regularexp.lisp
@@ -5,7 +5,7 @@
   ((precedence :accessor precedence :initarg :precedence)))
 (defmethod bracket ((obj pattern) outer-precedence)
   (if (< (precedence obj) outer-precedence)
-      (format nil "( ~a )" (to-s obj))
+      (format nil "(~a)" (to-s obj))
       (to-s obj)))
 
 ;;emptyクラス
@@ -14,7 +14,7 @@
 (defmethod initialize-instance :after ((empty empty) &key)
   (with-slots (precedence) empty
     (setf precedence 3)
-    (format t "()")))
+    (format nil "()")))
 (defmacro make-empty ()
   `(make-instance 'empty))
 
@@ -27,7 +27,7 @@
 (defmethod initialize-instance :after ((literal literal) &key)
   (with-slots (precedence char) literal
     (setf precedence 3)
-    (format t "~a" (to-s literal))))
+    (format nil "~a" (to-s literal))))
 (defmacro make-literal (char)
   `(make-instance 'literal :char ,char))
 
@@ -35,6 +35,10 @@
 (defclass concat (pattern)
   ((first :accessor fst :initarg :fst)
    (second :accessor scnd :initarg :scnd)))
+(defmethod initialize-instance :after ((concat concat) &key)
+  (with-slots (precedence fst scnd) concat
+    (setf precedence 1)
+    (format nil "~a" (to-s concat))))
 (defmethod to-s ((obj concat))
   (format nil "~a~a"
           (bracket (fst obj) (precedence (fst obj)))
@@ -43,4 +47,29 @@
   `(make-instance 'concat :fst ,fst :scnd ,scnd))
 
 
-;;
+;;chooseクラス
+(defclass choose (pattern)
+  ((first :accessor fst :initarg :fst)
+   (second :accessor scnd :initarg :scnd)))
+(defmethod initialize-instance :after ((choose choose) &key)
+  (with-slots (precedence fst scnd) choose
+    (setf precedence 0)
+    (format nil "~a" (to-s choose))))
+(defmethod to-s ((obj choose))
+  (format nil "~a|~a"
+          (bracket (fst obj) (precedence (fst obj)))
+          (bracket (scnd obj) (precedence (scnd obj)))))
+(defmacro make-choose (fst scnd)
+  `(make-instance 'choose :fst ,fst :scnd ,scnd))
+
+;;repeatクラス
+(defclass repeat (pattern)
+  ((pattern :accessor pattern :initarg :pattern)))
+(defmethod initialize-instance :after ((repeat repeat) &key)
+  (with-slots (precedence pattern) repeat
+    (setf precedence 2)
+    (format nil "~a" (to-s repeat))))
+(defmethod to-s ((obj repeat))
+  (format nil "~a*" (bracket (pattern obj) (precedence obj))))
+(defmacro make-repeat (pattern)
+  `(make-instance 'repeat :pattern ,pattern))
