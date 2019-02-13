4ec429cca96d15c97e579c6d3bc3a0228c81ed16
nisi3genki <nisi3genki@gmail.com>
2019-02-13 19:34:23 +0900

    自由移動に対応

--- nfa.lisp
+++ nfa.lisp
@@ -6,13 +6,24 @@
 (defun appliablep (rule state char)
   (and
    (= state (farule-state rule))
-   (char= char (farule-char rule))))
+   (if (characterp char)
+       (if (farule-char rule)
+           (char= char (farule-char rule)))
+       (if (farule-char rule)
+           nil
+           t))))
 (defun follow (rule) (farule-next-state rule))
+
+(defun follow-free-moves (book states)
+  (let ((more-states (next-states book states nil)))
+    (if (subsetp more-states states)
+        states
+        (follow-free-moves book (union states more-states)))))
 
 ;;nfaの遷移規則を生成
 (defun next-states (book states char)
   (mapcan (lambda (state) (follow-rules-for book state char))
-           states))
+          states))
 (defun follow-rules-for (rules state char)
   (mapcar #'follow (rules-for rules state char)))
 (defun rules-for (rules state char)
@@ -29,10 +40,14 @@
   (if (intersection (nfa-curr-state nfa) (nfa-accept-state nfa))
       t))
 
+(defun curr-states (nfa)
+  (sort (follow-free-moves (nfa-book nfa) (nfa-curr-state nfa))
+        #'<))
+
 (defun read-character (nfa char)
   (setf (nfa-curr-state nfa)
-        (next-states (nfa-book nfa) (nfa-curr-state nfa) char)))
-(defun read-string (nfa string)
-  (let ((temp nfa))
-    (map 'list (lambda (c) (read-character nfa c)) string)
+        (next-states (nfa-book nfa) (curr-states nfa) char)))
+(defun read-string (nfa1 string)
+  (let ((temp nfa1))
+    (map 'list (lambda (c) (read-character temp c)) string)
     (acceptp temp)))
